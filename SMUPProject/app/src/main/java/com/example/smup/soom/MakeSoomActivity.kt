c2de9655b3416e740a5a9679133a4e249ba6f89f
kimjjunho <kimjjun1007@gmail.com>
2022-02-06 23:53:17 +0900

    init::2022.02.06

---
+++ SMUPProject/app/src/main/java/com/example/smup/soom/MakeSoomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smup.soom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeSoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_soom)
+    }
+}
