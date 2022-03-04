0b74105877e2608589cf15ae6e79a8b63609e1c9
kimjjunho <kimjjun1007@gmail.com>
2022-03-04 15:47:44 +0900

    init::LoginActivity

--- SMUPProject/app/src/main/java/com/example/smup/start/LoginActivity.kt
+++ SMUPProject/app/src/main/java/com/example/smup/start/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.example.smup.start
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smup.MainActivity
 import com.example.smup.R
+import com.example.smup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityLoginBinding
+    private val binding get() = mBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mBinding.loginBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
