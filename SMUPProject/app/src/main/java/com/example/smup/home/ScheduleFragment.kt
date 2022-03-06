adeadf6046375d0f4ebcb2343ee6893846f1bd02
kimjjunho <kimjjun1007@gmail.com>
2022-03-06 23:05:42 +0900

    init::ScheduleProgressbarSample

--- SMUPProject/app/src/main/java/com/example/smup/home/ScheduleFragment.kt
+++ SMUPProject/app/src/main/java/com/example/smup/home/ScheduleFragment.kt
@@ -5,9 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import com.example.smup.R
+import com.example.smup.databinding.FragmentMyPageBinding
+import com.example.smup.databinding.FragmentScheduleBinding
+import java.util.*
 
 class ScheduleFragment : Fragment() {
+
+    private var CurrentProgress = 0
+    private var progressBar: ProgressBar? = null
+    private var mBinding : FragmentScheduleBinding? = null
+    private val binding get() = mBinding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +24,26 @@ class ScheduleFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_schedule, container, false)
+        mBinding = FragmentScheduleBinding.inflate(inflater,container,false)
+        val view = binding.root
+        val binding = FragmentScheduleBinding.bind(view)
+
+        progressBar = binding.circleProgressbar
+
+        val timer = Timer()
+        val timerTask: TimerTask = object : TimerTask(){
+            override fun run() {
+                CurrentProgress = CurrentProgress + 1
+                progressBar!!.setProgress(CurrentProgress)
+                progressBar!!.setMax(60)
+                if(CurrentProgress>=60){
+                    CurrentProgress = 0
+                }
+            }
+        }
+        timer.schedule(timerTask, 0, 1000)
+
+        return view
     }
 
     companion object {
