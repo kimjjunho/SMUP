0d1e27b7944d4f7fb4638a23ea69267ec4687b73
kimjjunho <kimjjun1007@gmail.com>
2022-03-09 22:52:31 +0900

    init::dmsPage

--- SMUPProject/app/src/main/java/com/example/smup/dms/DMSFragment.kt
+++ SMUPProject/app/src/main/java/com/example/smup/dms/DMSFragment.kt
@@ -1,10 +1,13 @@
 package com.example.smup.dms
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.smup.MainActivity
 import com.example.smup.R
 
 class DMSFragment : Fragment() {
@@ -12,10 +15,12 @@ class DMSFragment : Fragment() {
         super.onCreate(savedInstanceState)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val mainActivity  = activity as MainActivity
+        val dialog = Dialog(mainActivity)
+        dialog.setContentView(R.layout.dialog_dms)
+        dialog.show()
+
         return inflater.inflate(R.layout.fragment_d_m_s, container, false)
     }
 
