f77c16b355ec5d5b0b97c2dd71b241c63db060de
kimjjunho <kimjjun1007@gmail.com>
2022-03-22 22:25:49 +0900

    init::LoginViewModel

--- SMUPProject/app/src/main/java/com/example/smup/Constans.kt
+++ SMUPProject/app/src/main/java/com/example/smup/Constans.kt
@@ -1,4 +1,6 @@
 package com.example.smup.feature
 
 val BASE_URL = "http://3.36.108.34:8080/api/v1/"
+var ACCESS_TOKEN:String = ""
+var REFRESH_TOKEN:String = ""
 
--- SMUPProject/app/src/main/java/com/example/smup/feature/login/viewmodel/LoginViewModel.kt
+++ SMUPProject/app/src/main/java/com/example/smup/feature/login/viewmodel/LoginViewModel.kt
@@ -1,10 +1,28 @@
 package com.example.smup.feature.login.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.smup.data.login.LoginRepository
+import com.example.smup.feature.ACCESS_TOKEN
+import com.example.smup.feature.REFRESH_TOKEN
+import com.example.smup.feature.login.model.LoginRequest
 
 class LoginViewModel(
     private val rp:LoginRepository
 ):ViewModel(){
+    val success : MutableLiveData<Boolean> = MutableLiveData()
+    val faild : MutableLiveData<Boolean> = MutableLiveData()
 
+    fun login(loginRequest: LoginRequest){
+        rp.login(loginRequest).subscribe { response ->
+            if(response.isSuccessful){
+                ACCESS_TOKEN = "Bearer " + response.body()?.accessToken.toString()
+                REFRESH_TOKEN = response.body()?.refreshToken.toString()
+                success.value = true
+            }else{
+                faild.value = true
+            }
+
+        }
+    }
 }
