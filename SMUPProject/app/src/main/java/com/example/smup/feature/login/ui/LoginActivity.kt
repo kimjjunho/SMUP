9a317f6041416a90049dffa7347e30cebb0b346d
kimjjunho <kimjjun1007@gmail.com>
2022-03-23 21:23:59 +0900

    init::LoginObserveEvent

--- SMUPProject/app/src/main/java/com/example/smup/feature/login/ui/LoginActivity.kt
+++ SMUPProject/app/src/main/java/com/example/smup/feature/login/ui/LoginActivity.kt
@@ -3,29 +3,51 @@ package com.example.smup.feature.login.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
 import com.example.smup.R
 import com.example.smup.databinding.ActivityLoginBinding
 import com.example.smup.feature.MainActivity
+import com.example.smup.feature.login.model.LoginRequest
+import com.example.smup.feature.login.viewmodel.LoginViewModel
 import com.example.smup.feature.makeid.MakeIdActivity
+import org.koin.androidx.viewmodel.compat.ScopeCompat.viewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.sign
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var mBinding : ActivityLoginBinding
-    private val binding get() = mBinding
+    private lateinit var dBinding : ActivityLoginBinding
+    private val binding get() = dBinding
+    val vm : LoginViewModel by viewModel()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
-        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        dBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
         setContentView(binding.root)
 
-        mBinding.signBtn.setOnClickListener {
+        dBinding.signBtn.setOnClickListener {
             startActivity(Intent(this, MakeIdActivity::class.java))
         }
-        mBinding.loginBtn.setOnClickListener {
+        dBinding.loginBtn.setOnClickListener {
+            val textId = dBinding.textId.text.toString()
+            val textPass = dBinding.textPass.text.toString()
+            vm.login(LoginRequest("deviceToken",textId,textPass))
+
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        observeEvent()
+    }
+
+    fun observeEvent(){
+        vm.success.observe(this@LoginActivity,{
+                it.run {
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                }
+        })
     }
 }
