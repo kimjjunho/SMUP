da49716bb6199d63042b122ca59967270d438767
kimjjunho <kimjjun1007@gmail.com>
2022-03-29 22:47:04 +0900

    init::MealDate

--- SMUPProject/app/src/main/java/com/example/smup/feature/home/meal/ui/MealFragment.kt
+++ SMUPProject/app/src/main/java/com/example/smup/feature/home/meal/ui/MealFragment.kt
@@ -1,21 +1,26 @@
 package com.example.smup.feature.home.meal.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.smup.data.home.MealRepository
 import com.example.smup.databinding.FragmentMealBinding
-import com.example.smup.feature.MainActivity
 import com.example.smup.feature.home.meal.viewmodel.MealViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MealFragment : Fragment() {
+    val TAG = "MealFragment"
 
     private lateinit var dBinding : FragmentMealBinding
     private val binding get() = dBinding
     val vm : MealViewModel by viewModel()
+
+    var click = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,17 @@ class MealFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dBinding = FragmentMealBinding.inflate(inflater, container, false)
         val view = binding.root
-        val binding = FragmentMealBinding.bind(view)
+
+        //vm.getTodayMeal()
+
+        dBinding.rightImage.setOnClickListener {
+            vm.addData()
+            //vm.getMeal(date.toString())
+        }
+        dBinding.leftIamge.setOnClickListener {
+            vm.minusData()
+            //vm.getMeal(date.toString())
+        }
 
         observeEvent()
 
@@ -37,26 +52,34 @@ class MealFragment : Fragment() {
     }
 
     fun observeEvent(){
-        val view = binding.root
-        val binding = FragmentMealBinding.bind(view)
-
         activity?.let {
             vm.breakfast.observe(it,{
                 it.run{
-                    binding.firstTV.text = it
+                    dBinding.firstTV.text = it
                 }
             })
             vm.lunch.observe(it,{
                 it.run {
-                    binding.secondTV.text = it
+                    dBinding.secondTV.text = it
                 }
             })
             vm.dinner.observe(it,{
                 it.run {
-                    binding.thirdTv.text = it
+                    dBinding.thirdTv.text = it
+                }
+            })
+            vm.day.observe(it,{
+                it.run{
+                    dBinding.dayText.text = it+"요일"
+                }
+            })
+            vm.date.observe(it,{
+                it.run {
+                    dBinding.dateText.text = it
                 }
             })
         }
 
     }
+
 }
--- SMUPProject/app/src/main/java/com/example/smup/feature/home/meal/viewmodel/MealViewModel.kt
+++ SMUPProject/app/src/main/java/com/example/smup/feature/home/meal/viewmodel/MealViewModel.kt
@@ -1,16 +1,24 @@
 package com.example.smup.feature.home.meal.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.smup.data.home.MealRepository
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MealViewModel (
     private val rp: MealRepository
 ): ViewModel(){
-    
+    val TAG = "MealViewModel"
+
     val breakfast : MutableLiveData<String> = MutableLiveData()
     val lunch : MutableLiveData<String> = MutableLiveData()
     val dinner : MutableLiveData<String> = MutableLiveData()
+    val date : MutableLiveData<String> = MutableLiveData()
+    val day : MutableLiveData<String> = MutableLiveData()
+    var click = 0
 
 
     fun getTodayMeal(){
@@ -41,5 +49,42 @@ class MealViewModel (
                     dinner.value = "와이파이"
                 }
             }
+    }
+
+    fun dayData(){
+        val cal = Calendar.getInstance()
+        cal.time = Date()
+
+        val df1: DateFormat = SimpleDateFormat("yyyyMMdd")
+        cal.add(Calendar.DATE,click)
+
+        val df2: DateFormat = SimpleDateFormat("yyyyMMdd")
+        val date : Date = df2.parse(df1.format(cal.time))
+
+        val df3:DateFormat = SimpleDateFormat("yyyy.MM.dd")
+
+        val cal2 = Calendar.getInstance()
+        cal2.time = date
+
+        when(cal2.get(Calendar.DAY_OF_WEEK)){
+            1-> day.value = "일"
+            2-> day.value = "월"
+            3-> day.value = "화"
+            4-> day.value = "수"
+            5-> day.value = "목"
+            6-> day.value = "금"
+            7-> day.value = "토"
+        }
+
+        this.date.value = df3.format(cal.time)
+    }
+
+    fun minusData(){
+        click--
+        dayData()
+    }
+    fun addData(){
+        click++
+        dayData()
     }
 }
